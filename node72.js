e455eccca7e066acaab88bf21535ac49bee61170
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-07 12:56:00 +0530

    Delete node72.js

--- node72.js
+++
@@ -1,5 +0,0 @@
-// Arrow function that takes two parameters and returns their sum
-const add = (a, b) => a + b;
-
-// Calling the arrow function and logging the result
-console.log(add(5, 10)); // Output: 15
