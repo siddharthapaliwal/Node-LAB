00cf76d6ae4b617f9dfb27439de9aa0f6e5c7ec7
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-07 12:25:30 +0530

    Delete node48.js

--- node48.js
+++
@@ -1,8 +0,0 @@
-// Define an array with at least six elements
-const numbers = [10, 20, 30, 40, 50, 60, 70, 80];
-
-// Calculate the length of the array
-const arrayLength = numbers.length;
-
-// Display the length of the array
-console.log(`The length of the array is: ${arrayLength}`);
