4e6b3b4c2a0803c0f9a560d7973de18c3f2b7ff0
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-07 12:56:26 +0530

    Delete node74.js

--- node74.js
+++
@@ -1,9 +0,0 @@
-// Destructuring assignment with an object
-const person = { name: "John", age: 30, city: "New York" };
-
-// Destructuring the object into variables
-const { name, age, city } = person;
-
-console.log(name);  // Output: John
-console.log(age);   // Output: 30
-console.log(city);  // Output: New York
