e39578b04b1eb4407307d50de1f34a932348e4bf
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-07 12:27:25 +0530

    Delete node53.js

--- node53.js
+++
@@ -1,9 +0,0 @@
-// Define two separate arrays
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-
-// Merge the arrays using the concat method
-const mergedArray = array1.concat(array2);
-
-// Log the merged array to the console
-console.log('Merged Array:', mergedArray);
