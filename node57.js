debec61c3096320a9750dff1041d9e2be7f4cb6c
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-07 12:28:29 +0530

    Delete node57.js

--- node57.js
+++
@@ -1,9 +0,0 @@
-// Create an object representing a book
-const book = {
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
-    year: 1960
-};
-
-// Log the object to the console
-console.log('Book Object:', book);
