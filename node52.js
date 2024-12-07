eb287838382f93a8e812a1c4f71e6196c669bf7a
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-07 12:27:12 +0530

    Delete node52.js

--- node52.js
+++
@@ -1,8 +0,0 @@
-// Define an array of string names
-const names = ['Charlie', 'Alice', 'Bob', 'Eve', 'Dave'];
-
-// Sort the array alphabetically using the sort method
-const sortedNames = names.sort();
-
-// Display the sorted array
-console.log('Sorted names:', sortedNames);
